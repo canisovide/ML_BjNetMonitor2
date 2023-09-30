682563d596b1ce95b10356c738a9bf6a665049c2
canisovide <sovidecanisius@gmail.com>
2023-09-30 19:04:44 +0100

    second commit

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+ML_BjNetMonitor
+
+Ce projet est un projet de machine Learning qui vise à se baser sur les données de
+la plateforme BjnetMonitor de l'entreprise EMES SARL pour faire des prédictions sur
+les performances de l'internet dans un réseau WAN. Il permettra de faire des analyses
+sur les performances du réseau.
+
+Ce projet vise à aider les administrateurs réseau et les utilisateurs
+à connaitre l'état de leurs réseaux et à proposer des solutions sur comment
+optimiser leurs réseaux wan.
+
+
+Auteur:
+    Votre Nom <votre@email.com>
+    Ronald SOVIDE ronald.sovide@gmail.com
+    Didier ESSOU
+Licence:
+
+
+Usage:
+    [Instructions pour exécuter ou utiliser le projet]
+
+Modules:
+
+Version : 1.1
+"""
+
+import pandas as pd
+
